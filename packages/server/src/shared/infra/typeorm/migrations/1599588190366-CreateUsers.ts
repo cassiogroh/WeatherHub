9aaaf63e79165c7a26b9119c43a63617f2c8d79e
cassiogroh <cassio_groh@hotmail.com>
2020-10-10 19:01:31 -0300

    fix: users migration export default

--- packages/server/src/shared/infra/typeorm/migrations/1599588190366-CreateUsers.ts
+++ packages/server/src/shared/infra/typeorm/migrations/1599588190366-CreateUsers.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1599588190366 implements MigrationInterface {
+export default class CreateUsers1599588190366 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(

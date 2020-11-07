46374978c07c66e8ffe27f193977dc54a1176b70
cassiogroh <cassio_groh@hotmail.com>
2020-11-07 17:16:37 -0300

    fix: show -- when property is missing

--- packages/server/src/modules/users/services/LoadStationsService/PopulateStations.ts
+++ packages/server/src/modules/users/services/LoadStationsService/PopulateStations.ts
@@ -89,17 +89,17 @@ export default class PopulateStations{
         station.neighborhood = data.value.observations[0].neighborhood;
         station.stationID = data.value.observations[0].stationID;
         station.url = `http://www.wunderground.com/personal-weather-station/dashboard?ID=${station.stationID}`
-        station.dewpt = dewpt || dewpt === 0 ? dewpt.toFixed(1) : null;
-        station.humidity = humidity || humidity === 0 ? humidity.toFixed(1) : null;
-        station.elev = elev || elev === 0 ? elev.toFixed(1) : null;
-        station.heatIndex = heatIndex || heatIndex === 0 ? heatIndex.toFixed(1) : null;
-        station.precipRate = precipRate || precipRate === 0 ? precipRate.toFixed(1) : null;
-        station.precipTotal = precipTotal || precipTotal === 0 ? precipTotal.toFixed(1) : null;
-        station.pressure = pressure || pressure === 0 ? pressure.toFixed(1) : null;
-        station.temp = temp || temp === 0 ? temp.toFixed(1) : null;
-        station.windChill = windChill || windChill === 0 ? windChill.toFixed(1) : null;
-        station.windGust = windGust || windGust === 0 ? windGust.toFixed(1) : null;
-        station.windSpeed = windSpeed || windSpeed === 0 ? windSpeed.toFixed(1) : null;
+        station.dewpt = dewpt || dewpt === 0 ? dewpt.toFixed(1) : '--';
+        station.humidity = humidity || humidity === 0 ? humidity.toFixed(1) : '--';
+        station.elev = elev || elev === 0 ? elev.toFixed(1) : '--';
+        station.heatIndex = heatIndex || heatIndex === 0 ? heatIndex.toFixed(1) : '--';
+        station.precipRate = precipRate || precipRate === 0 ? precipRate.toFixed(1) : '--';
+        station.precipTotal = precipTotal || precipTotal === 0 ? precipTotal.toFixed(1) : '--';
+        station.pressure = pressure || pressure === 0 ? pressure.toFixed(1) : '--';
+        station.temp = temp || temp === 0 ? temp.toFixed(1) : '--';
+        station.windChill = windChill || windChill === 0 ? windChill.toFixed(1) : '--';
+        station.windGust = windGust || windGust === 0 ? windGust.toFixed(1) : '--';
+        station.windSpeed = windSpeed || windSpeed === 0 ? windSpeed.toFixed(1) : '--';
         station.status = 'online';
 
         if (!userId) {
@@ -178,29 +178,29 @@ export default class PopulateStations{
             humidityHigh
           } = historicData;
   
-          stationHistoric.humidityHigh = humidityHigh || humidityHigh === 0 ? humidityHigh.toFixed(1) : null;
-          stationHistoric.humidityLow = humidityLow || humidityLow === 0 ? humidityLow.toFixed(1) : null;
-          stationHistoric.humidityAvg = humidityAvg || humidityAvg === 0 ? humidityAvg.toFixed(1) : null;
-          stationHistoric.tempHigh = tempHigh || tempHigh === 0 ? tempHigh.toFixed(1) : null;
-          stationHistoric.tempLow = tempLow || tempLow === 0 ? tempLow.toFixed(1) : null;
-          stationHistoric.tempAvg = tempAvg || tempAvg === 0 ? tempAvg.toFixed(1) : null;
-          stationHistoric.windspeedHigh = windspeedHigh || windspeedHigh === 0 ? windspeedHigh.toFixed(1) : null;
-          stationHistoric.windspeedLow = windspeedLow || windspeedLow === 0 ? windspeedLow.toFixed(1) : null;
-          stationHistoric.windspeedAvg = windspeedAvg || windspeedAvg === 0 ? windspeedAvg.toFixed(1) : null;
-          stationHistoric.windgustHigh = windgustHigh || windgustHigh === 0 ? windgustHigh.toFixed(1) : null;
-          stationHistoric.windgustLow = windgustLow || windgustLow === 0 ? windgustLow.toFixed(1) : null;
-          stationHistoric.windgustAvg = windgustAvg || windgustAvg === 0 ? windgustAvg.toFixed(1) : null;
-          stationHistoric.dewptHigh = dewptHigh || dewptHigh === 0 ? dewptHigh.toFixed(1) : null;
-          stationHistoric.dewptLow = dewptLow || dewptLow === 0 ? dewptLow.toFixed(1) : null;
-          stationHistoric.dewptAvg = dewptAvg || dewptAvg === 0 ? dewptAvg.toFixed(1) : null;
-          stationHistoric.windchillHigh = windchillHigh || windchillHigh === 0 ? windchillHigh.toFixed(1) : null;
-          stationHistoric.windchillLow = windchillLow || windchillLow === 0 ? windchillLow.toFixed(1) : null;
-          stationHistoric.windchillAvg = windchillAvg || windchillAvg === 0 ? windchillAvg.toFixed(1) : null;
-          stationHistoric.heatindexHigh = heatindexHigh || heatindexHigh === 0 ? heatindexHigh.toFixed(1) : null;
-          stationHistoric.heatindexLow = heatindexLow || heatindexLow === 0 ? heatindexLow.toFixed(1) : null;
-          stationHistoric.heatindexAvg = heatindexAvg || heatindexAvg === 0 ? heatindexAvg.toFixed(1) : null;
-          stationHistoric.pressureMax = pressureMax || pressureMax === 0 ? pressureMax.toFixed(1) : null;
-          stationHistoric.pressureMin = pressureMin || pressureMin === 0 ? pressureMin.toFixed(1) : null;
+          stationHistoric.humidityHigh = humidityHigh || humidityHigh === 0 ? humidityHigh.toFixed(1) : '--';
+          stationHistoric.humidityLow = humidityLow || humidityLow === 0 ? humidityLow.toFixed(1) : '--';
+          stationHistoric.humidityAvg = humidityAvg || humidityAvg === 0 ? humidityAvg.toFixed(1) : '--';
+          stationHistoric.tempHigh = tempHigh || tempHigh === 0 ? tempHigh.toFixed(1) : '--';
+          stationHistoric.tempLow = tempLow || tempLow === 0 ? tempLow.toFixed(1) : '--';
+          stationHistoric.tempAvg = tempAvg || tempAvg === 0 ? tempAvg.toFixed(1) : '--';
+          stationHistoric.windspeedHigh = windspeedHigh || windspeedHigh === 0 ? windspeedHigh.toFixed(1) : '--';
+          stationHistoric.windspeedLow = windspeedLow || windspeedLow === 0 ? windspeedLow.toFixed(1) : '--';
+          stationHistoric.windspeedAvg = windspeedAvg || windspeedAvg === 0 ? windspeedAvg.toFixed(1) : '--';
+          stationHistoric.windgustHigh = windgustHigh || windgustHigh === 0 ? windgustHigh.toFixed(1) : '--';
+          stationHistoric.windgustLow = windgustLow || windgustLow === 0 ? windgustLow.toFixed(1) : '--';
+          stationHistoric.windgustAvg = windgustAvg || windgustAvg === 0 ? windgustAvg.toFixed(1) : '--';
+          stationHistoric.dewptHigh = dewptHigh || dewptHigh === 0 ? dewptHigh.toFixed(1) : '--';
+          stationHistoric.dewptLow = dewptLow || dewptLow === 0 ? dewptLow.toFixed(1) : '--';
+          stationHistoric.dewptAvg = dewptAvg || dewptAvg === 0 ? dewptAvg.toFixed(1) : '--';
+          stationHistoric.windchillHigh = windchillHigh || windchillHigh === 0 ? windchillHigh.toFixed(1) : '--';
+          stationHistoric.windchillLow = windchillLow || windchillLow === 0 ? windchillLow.toFixed(1) : '--';
+          stationHistoric.windchillAvg = windchillAvg || windchillAvg === 0 ? windchillAvg.toFixed(1) : '--';
+          stationHistoric.heatindexHigh = heatindexHigh || heatindexHigh === 0 ? heatindexHigh.toFixed(1) : '--';
+          stationHistoric.heatindexLow = heatindexLow || heatindexLow === 0 ? heatindexLow.toFixed(1) : '--';
+          stationHistoric.heatindexAvg = heatindexAvg || heatindexAvg === 0 ? heatindexAvg.toFixed(1) : '--';
+          stationHistoric.pressureMax = pressureMax || pressureMax === 0 ? pressureMax.toFixed(1) : '--';
+          stationHistoric.pressureMin = pressureMin || pressureMin === 0 ? pressureMin.toFixed(1) : '--';
   
           station.push(stationHistoric);
         })
